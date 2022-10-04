e8858695627abe028b3f86cdf11714721e173dc4
Ivan Ramos <huramos777@gmail.com>
2022-10-04 14:32:55 -0300

    Finalizado

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
-export default function Card(props) {
+export default function Card({ max, min, name, img, onClose}) { //se peude hacer destructuring porque es un objeto lo que se recibe
   // acá va tu código
-  return <div>Card Component</div>
+  return (<div>
+    <button onClick={onClose}>X</button>
+    <h4>{name}</h4>
+    <div>
+    <p>Max</p>
+    <p>{max}</p>
+    <p>Min</p>
+    <p>{min}</p>
+    
+    </div>
+    <img src={`http://openweathermap.org/img/wn/${img}@2x.png`}
+          alt='icono clima' />
+  </div>);
 };
--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Card from './Card';
+
+
 
 export default function Cards(props) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+  const ciudades = props.cities;
+  return (<div>
+            {
+              ciudades.map(obj => <Card
+                                        key={obj.id} //  siempre pasarle un prop key para que react pueda diferenciarlos
+                                        name={obj.name} 
+                                        min={obj.main.temp_min}
+                                        max={obj.main.temp_max}
+                                        img={obj.weather[0].icon}
+                                        boton={() => alert(obj.name)} />)
+            }  
+          </div>
+  );
 };
